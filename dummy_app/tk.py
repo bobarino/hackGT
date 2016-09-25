5cf23439f45f02572660be779261db775f6c5dd5
John Bonaguro <jbonaguro2@gmail.com>
2016-09-25 01:51:34 -0400

    Finished Dummy App

--- dummy_app/tk.py
+++ dummy_app/tk.py
@@ -1,17 +1,50 @@
 import sys
+import requests
+import datetime
+import json
 
 if sys.version_info.major < 3:
     import Tkinter as tkinter
 else:
     import tkinter
 
+apiKey = 'e20498f24d98edbea7cb726685b02566'
+accountID = '57e624c6dbd83557146123b9'
+merchantID = '57e62a1cdbd83557146123bd'
+
+app = tkinter.Tk()
+app.title("SPEND MONEY")
+amount = tkinter.StringVar()
+accountNum = tkinter.StringVar()
+
+f = tkinter.Frame(app)
+sf2 = tkinter.Frame(f)
+sf3 = tkinter.Frame(f)
+
+_amount = tkinter.Entry(sf2, justify=tkinter.CENTER, textvariable=amount)
+_accountNum = tkinter.Entry(sf3, justify=tkinter.CENTER, textvariable=accountNum)
+
 def submitRequest():
-    pass
+    amt = float(amount.get())
+    account_id = accountNum.get()
+    
+    now = datetime.datetime.now()
+    purchaseData = {
+                    'merchant_id':merchantID,
+                    'medium':'balance',
+                    'purchase_date':str(now),
+                    'amount':amt,
+                    'description':'Payout',
+                    }
+    r = requests.post('http://api.reimaginebanking.com/accounts/'+account_id
+                          +'/purchases?key='+apiKey,
+                      data = json.dumps(purchaseData),
+                      headers = {'content-type':'application/json'})
+    print(r.text)
+
 
 img = []
 def buildapp():
-    app = tkinter.Tk()
-    f = tkinter.Frame(app)
     f.pack()
     sf = tkinter.Frame(f)
     img = tkinter.PhotoImage(file="logo.gif")
@@ -19,16 +52,23 @@ def buildapp():
     banner.image = img
     banner.pack()
     sf.pack(padx=15, pady=15)
-    label = tkinter.Label(f, text="WELCOME TO E-CORP")
-    label.pack()
-    s = tkinter.Entry(f, justify=tkinter.CENTER)
-    s.pack()
-    sf2 = tkinter.Frame(f)
-    sf2.pack()
-    #label = tkinter.Label(sf2, text="DIRECTION")
-    #label.pack()
-    submit = tkinter.Button(sf2, text="Submit")#, command=submitRequest)
-    submit.pack(expand=True)
+    title = tkinter.Label(f, text="WELCOME TO SPENDBUCKS COFFEE")
+    title.pack(padx=15, pady=15)
+
+    subtitle = tkinter.Label(f, text="Please Enter your Total and Bank Account ID:")
+    subtitle.pack(padx=15, pady=15)
+    
+    sf2.pack(padx=15, pady=15)
+    ds = tkinter.Label(sf2, text="$")
+    ds.pack(side=tkinter.LEFT)
+    _amount.pack()
+    
+    sf3.pack(padx=15, pady=15)
+    ns = tkinter.Label(sf3, text="#")
+    ns.pack(side=tkinter.LEFT)
+    _accountNum.pack()
+    submit = tkinter.Button(f, text="Submit", command=submitRequest)
+    submit.pack(expand=True, padx=15, pady=15)
     return app;
 
 app = buildapp()
